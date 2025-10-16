00ad3dd62e30d77d845cbfb3095fb8410050476e
nlpusosua <144300996+nlpusosua@users.noreply.github.com>
2025-10-16 22:18:47 +0700

    Initial commit: Setup backend and frontend projects

---
+++ backend/src/main/java/com/example/IOT_SmartStick/IotSmartStickApplication.java
@@ -0,0 +1,13 @@
+package com.example.IOT_SmartStick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSmartStickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSmartStickApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/IOT_SmartStick/IotSmartStickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IOT_SmartStick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSmartStickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
